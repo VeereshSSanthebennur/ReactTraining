c4084516dbd6a01cfd63d4e32ed66bafd6e8cc77
Veeresh Shankar <VeereshShankar978978@outlook.com>
2023-01-15 21:55:30 +0530

    Trial and error

--- reacttraining/src/App.js
+++ reacttraining/src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/VeereshSSanthebennur"
           target="_blank"
           rel="noopener noreferrer"
         >
